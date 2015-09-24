25764e65967b24f04318a140e70b79b5827b25ab
Amanze Ogbonna <darlingtonamz@gmail.com>
2015-09-24 18:23:23 +0000

    added Coffee.java and CoffeeMaker.java

---
+++ Coffee.java
@@ -0,0 +1,28 @@
+
+public class Coffee {
+	private int milkAmt, coffeeAmt, waterAmt, chocolateAmt;
+	
+	public Coffee(int milk, int coffee, int water, int chocolate){
+		milkAmt = milk;
+		coffeeAmt = coffee;
+		waterAmt = water;
+		chocolateAmt = chocolate;
+	}
+
+	public int getMilkAmt() {
+		return milkAmt;
+	}
+
+	public int getCoffeeAmt() {
+		return coffeeAmt;
+	}
+
+	public int getWaterAmt() {
+		return waterAmt;
+	}
+
+	public int getChocolateAmt() {
+		return chocolateAmt;
+	}
+	
+}
---
+++ CofferMaker.java
@@ -0,0 +1,20 @@
+
+public class CofferMaker {
+	private int milkAmt, coffeeAmt, waterAmt, chocolateAmt;
+	
+	public static void main(String[] args){
+		System.out.println("Working");
+	}
+	
+	public void mix(){
+		
+	}
+	
+	public void load(){
+		
+	}
+	
+	public void brew(){
+		
+	}
+}
